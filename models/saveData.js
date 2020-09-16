868d09c7f8915ae0000d315fac120b68b8ef402b
Jordan Muncrief <jordean89@gmail.com>
2020-09-16 14:30:48 -0600

    Fixed Sequelize version error. Created saveData DB model.

---
+++ models/saveData.js
@@ -0,0 +1,37 @@
+// Model for Data table
+module.exports = function (sequelize, DataTypes) {
+    const saveData = sequelize.define("saveData", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        open: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        close: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        volume: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        }
+    });
+    // Method stored on Data model checks if currently logged-in user (email) matches the "email" field of the current row
+    saveData.prototype.isCurrentUser = (user) => {
+        if (this.email === user) {
+            return true;
+        } else return false;
+    };
+
+    return saveData;
+};
